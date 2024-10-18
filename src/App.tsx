92a68542cd57545c20d16eed7ae0b9bbb543b0c4
Mark Daniel Reyes <mare044@student.kristiania.no>
2024-10-18 09:45:36 +0200

    changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,15 @@
 import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap";
+import Image from "./Image";
+
 const App = () => {
-  return <h1>hei</h1>;
+  return (
+    <>
+      <h1 className="text-center bold bg-black text-white m-4">Portfolio</h1>
+      <Image />
+    </>
+  );
 };
 
 export default App;
